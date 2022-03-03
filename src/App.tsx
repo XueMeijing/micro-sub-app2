a480b3647faed50794d8cab8d947440b71053f0b
薛腾飞 <xuetf@maycur.com>
2022-03-03 19:35:57 +0800

    修改通信方式

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
 import { IonApp } from '@ionic/react';
 import Routers from './router/router';
 import { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 function App() {
   const navigate = useNavigate()
+  const subLocation2 = useLocation()
+  console.log('subLocation2', subLocation2)
+
   useEffect(() => {
+    console.log('micro-sub-app2 useEffect')
     // 监听基座下发的数据变化
     const dataListener = (data: any) => {
-      const { type, data: receivedData } = data
-      switch (type) {
-        case 'micro-app-react':
-          navigate(receivedData.path)
+      switch (data.type) {
+        case 'switchTab':
+          console.log('micro-sub-app2 switchTab')
+          navigate(data.path)
           return
       }
-
     }
-    (window as any).microApp && (window as any).microApp.addGlobalDataListener(dataListener, true)
+    (window as any).microApp && (window as any).microApp.addDataListener(dataListener)
   }, [])
+
   return (
     <div className="App">
-      microReact
+      <Routers />
     </div>
   );
 }
